621503eb9b56150d20e21ae0ddca3c73cf446645
Ana Paredes <annaparedes011@gmail.com>
2024-04-17 15:10:02 -0300

    pagina ingles update

--- js/app.js
+++ js/app.js
@@ -1,24 +1,67 @@
+const languageButton = document.getElementById('languageButton');
+const dropdownMenu = document.querySelector('.dropdown-menu');
 
-// Selecciona el elemento checkbox con la clase "check"
-var check = document.querySelector(".check");
 
-// Agrega un evento de clic al checkbox que ejecutará la función 'idioma'
-check.addEventListener('click', idioma);
+languageButton.addEventListener('click', () => {
+    languageButton.parentNode.classList.toggle('open');
+});
 
-// La función 'idioma' se ejecuta cuando el checkbox es clicado
-function idioma() {
-    // Verifica si el checkbox está seleccionado
-    let id = check.checked;
+
+document.addEventListener('click', (event) => {
+    if (!languageButton.parentNode.contains(event.target)) {
+        languageButton.parentNode.classList.remove('open');
+    }
+});
+
+
+dropdownMenu.addEventListener('click', (event) => {
+    event.preventDefault();
+    const selectedLanguage = event.target.getAttribute('data-lang');
     
-    // Redirige a una página específica según el estado del checkbox
-    if (id) {
-        // Si el checkbox está seleccionado, redirige a "es/index.html"
-        location.href = "es/index.html";
+    changeLanguage(selectedLanguage);
+    languageButton.parentNode.classList.remove('open');
+});
+
+function changeLanguage(lang) {
+    const currentPath = window.location.pathname;
+
+    if (lang === 'es') {
+        // Caso español
+        if (currentPath.includes('en/indexx.html')) {
+            location.href = '../index.html';
+        } else if (currentPath.includes('en/aboutt.html')) {
+            location.href = '../pages/about.html';
+        } else if (currentPath.includes('en/productss.html')) {
+            location.href = '../pages/products.html';
+        } else if (currentPath.includes('en/furniture.html')) {
+            location.href = '../pages/mobiliario.html';
+        } else if (currentPath.includes('en/textiless.html')) {
+            location.href = '../pages/textiles.html';
+        } else {
+            location.reload();
+        }
     } else {
-        // Si el checkbox no está seleccionado, redirige a "../index.html"
-        location.href = "index.html";
+        // Caso inglés
+        if (currentPath.includes('index.html')) {
+            location.href = 'en/indexx.html';
+        } else if (currentPath.includes('pages/about.html')) {
+            location.href = '../en/aboutt.html';
+        } else if (currentPath.includes('pages/products.html')) {
+            location.href = '../en/productss.html';
+        } else if (currentPath.includes('pages/mobiliario.html')) {
+            location.href = '../en/furniture.html';
+        } else if (currentPath.includes('pages/textiles.html')) {
+            location.href = '../en/textiless.html';
+        }
     }
 }
+
+
+
+
+
+
+
 
 
 
